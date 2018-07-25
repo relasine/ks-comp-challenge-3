365565ab64310f37aa4377109a3a8b35908794f5
Kevin Simpson <relasine@gmail.com>
2018-07-25 08:10:56 -0600

    Add first set of mqs

--- javascript.js
+++ javascript.js
@@ -1,16 +1,12 @@
+var collapseBtn = document.querySelector('.arrow-and-lines');
+var mainSection = document.querySelector('.main-section-div');
+var viewPort = document.querySelector('window');
 var sideNav = document.querySelector('.side-nav');
-var collapseBtn = document.querySelector('.arrow-and-lines')
-var body = document.querySelector('body');
-var topBar = document.querySelector('section');
-var article = document.querySelector('article');
-var main = document.querySelector('main');
-var header = document.querySelector('header');
-var aside = document.querySelector('aside');
 
 collapseBtn.addEventListener('click', toggleNav);
 
 function toggleNav() {
-  if (sideNav.style.display === "" || sideNav.style.display === "block") {
+  if (sideNav.style.display === "") {
     toggleOff();
   } else {
     toggleOn();
@@ -18,22 +14,13 @@ function toggleNav() {
 }
 
 function toggleOff() {
-  sideNav.
+    console.log("working");
   sideNav.style.display = "none";
-  body.style.gridTemplateColumns = "1fr 400px";
-  topBar.style.gridColumn = "1/3";
-  header.style.gridColumn = "1/3";
-  article.style.gridColumn = "1/3";
-  main.style.gridColumn = "1";
-  aside.style.gridColumn = "2";
-}
+  mainSection.style.gridColumn = "1/3"
+  }
+
 
 function toggleOn() {
+  mainSection.style.gridColumn = "2"
   sideNav.style.display = "";
-  body.style.gridTemplateColumns = "220px 1fr 400px";
-  topBar.style.gridColumn = "2/4";
-  header.style.gridColumn = "2/4";
-  article.style.gridColumn = "2/4";
-  main.style.gridColumn = "2";
-  aside.style.gridColumn = "3";
-}
+  }
