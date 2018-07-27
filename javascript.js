27d1b04df9e8f6492e802b931c80493df87e7422
Kevin Simpson <relasine@gmail.com>
2018-07-27 15:30:54 -0600

    Put combined CSS rules on multiple lines

--- javascript.js
+++ javascript.js
@@ -1,9 +1,12 @@
+var collapsePicBtn = document.querySelector('.border-arrow');
 var collapseBtn = document.querySelector('.arrow-and-lines');
-var mainSection = document.querySelector('.main-section-div');
+var mainSection = document.querySelector('main');
 var viewPort = document.querySelector('window');
 var sideNav = document.querySelector('.side-nav');
+var chartsDiv = document.querySelector('.charts');
 
 collapseBtn.addEventListener('click', toggleNav);
+collapsePicBtn.addEventListener('click', togglePics);
 
 function toggleNav() {
   if (sideNav.style.display === "") {
@@ -14,13 +17,31 @@ function toggleNav() {
 }
 
 function toggleOff() {
-    console.log("working");
   sideNav.style.display = "none";
   mainSection.style.gridColumn = "1/3"
   }
 
-
 function toggleOn() {
   mainSection.style.gridColumn = "2"
   sideNav.style.display = "";
+  }
+
+function togglePics() {
+  if (chartsDiv.style.display === "") {
+    togglePicsOff();
+  } else {
+    togglePicsOn();
+  }
+}
+
+function togglePicsOff() {
+  chartsDiv.style.display = "none";
+  collapsePicBtn.classList.add('fa-chevron-circle-down')
+  collapsePicBtn.classList.remove('fa-chevron-circle-up')
+  }
+
+function togglePicsOn() {
+  chartsDiv.style.display = "";
+  collapsePicBtn.classList.add('fa-chevron-circle-up')
+  collapsePicBtn.classList.remove('fa-chevron-circle-down')
   }
